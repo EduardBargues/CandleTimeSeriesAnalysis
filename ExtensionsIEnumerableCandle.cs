7f209514e8aa68980f4fc3be8cba9e3b9fcd1ca5
Eduard <eduardbargues@gmail.com>
2017-12-21 19:05:06 +0100

    - first commit

---
+++ ExtensionsIEnumerableCandle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TeslaAnalysis
+{
+    public static class ExtensionsIEnumerableCandle
+    {
+        public static CandleTimeSeries ToCandleTimeSeries(this IEnumerable<Candle> candles, string name = "")
+        {
+            return new CandleTimeSeries(candles)
+            {
+                Name = name,
+            };
+        }
+    }
+}
